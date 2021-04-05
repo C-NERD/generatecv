c7be9e675d4e5fc61728e4e71253cce26a722c61
C-NERD <alayaa694@gmail.com>
2021-04-05 19:10:44 +0100

    updated the code and changed the pdf style

--- README.md
+++ README.md
@@ -1,78 +1,14 @@
 # generatecv
-Generate cv is a simple python project which helps to generate tech cv(resume) using a single template like the cv [here](https://drive.google.com/file/d/1jMK9Wv8kcStDkrvsSbjHHkhoLCHjF_Ok/view?usp=sharing) if you like what you see consider hiring me.
+Generate cv is a simple python project which helps to generate tech cv(resume) using a single template like the cv [here](https://drive.google.com/file/d/1jMK9Wv8kcStDkrvsSbjHHkhoLCHjF_Ok/view?usp=sharing). If you like my qualifications on that cv you might want to consider hiring me 😆.
 
 The template the program used is inspired from cv advise given in these youtube videos
 - [How to create the perfect software developer resume](https://www.youtube.com/watch?v=GyjzOKdaioU&t=368s)
 - [How to make an amazing software development resume](https://www.youtube.com/watch?v=Xa1pFemaGyc&t=5s)
 
 # How to use the program
-1. After cloning the repo navigate into the root directory of the program and open the settings.json file
-2. Inside the settings.json file replace the value of the object with the key "title" with your name
-3. Then replace the values of the other fields with your information
+1. After cloning the repo, navigate into the projetc's root directory and open the settings.json file
+2. Inside the settings.json file replace the values of the objects with appropriate information
+3. Then open a terminal in the root directory and type in `pip3 install -r requirements.txt` to install the dependences
+3. Finally run `python3 cv.py` to generate your new CV(resume)
 
-example:
-
-if i want to add my email address in the contact section i would simply add
-`"Email:link mailto##alayaa694@gmail.com alayaa694@gmail.com"`
-what this does is it types the string "Email" to the cv followed by a link mailto:alayaa694@gmail.com and the string of the link as alayaa694@gmail.com
-*Remember to subtitute the `:` character in the link with the `##` *
-    
-The format for adding pure text to the cv is like so:
-
-`"Self taught software developer:text -july_2018"`
-- you type in the title of the information in this case it's "self taught software developer" 
-- then you'll add the `:` character
-- after which you'll type "text"
-- finally you'll type the text you want to add
-      
-The format for adding experience to the experience section in the cv is like so:
-
-`"Indie game developer:exp march_2019 till_date _ _ _ |Godot_|Python_|Blender"`
-- you type in you experience in this case it's "Indie game developer"
-- then you'll add the `:` character
-- after which you'll type "exp"
-- then you'll add the starting date of your experience
-- *To leave any portion empty simply type in a single _ character in it's place*
-- *Also to give space use the _ character*
-- then you'll type in the ending date of your work experience
-- followed by the country you had the experience 
-- the link to the companies website(remember to replace the `:` character in the link with `##`)
-- the name of the company
-- and finally the tools you used in such job
-      
-The format for adding skills to the skills sections of the cv is like so:
-
-`"Python:skill |Django_|Pandas_|PyQt5_|Pygame_|numpy_|Eel"`
-- You type in the name of the skill in this case "Python"
-- followed by the `:` character
-- then you type "skill"
-- after which you leave space and type in the tools you used for such skill      
-      
-The format for adding achievements to the achievements section of the cv is like so:
-
-`"Advent of code 2020:achieve I_participate_in_the_advent_of_code_2020_challenge https##//adventofcode.com/2020/about C-NERD"`
-- you type in the achievement in this case it's "Advent of code 2020"
-- then you'll add the `:` character
-- the you'll add "achieve"
-- follow it with space and type in your achievement
-- then leave a link to the said achievement(remember to replace the `:` character in the link with the `##` characters)
-- and finally type in the text for the link
-      
-      
-The format for adding projects to the project section of the cv is like so:
-
-`"Tiles:project A_2D_game_I_am_working_on_using_pygame _ _"`
-- you type the name of the project you are currently working on in this case it's "Tiles"
-- then input the `:` character and then type "project"
-- then add space and then type in a description of your project(keep it short)
-- then add the link to your project(remember to replace the `:` character with `##`)(if you don't have a link just add "_")
-- finally add the text to the link(if you didn't add add a link no need for this one)
-      
-      
-After filling your information in the settings.json file startup the virtual enviroment virtualenv, here is a link on how to use python virtual enviroments [link](https://uoa-eresearch.github.io/eresearch-cookbook/recipe/2014/11/26/python-virtual-env/#Activate%20the%20virtual%20environment)
-
-finally run
-`python3 cv.py`
-
-and you cv(resume) will be created in the same directory as the cv.py file
-have a nice job hunt.
+Your cv(resume) should be created in the project's root directory, have a nice job hunt.
---
+++ src/cv.py
@@ -0,0 +1,222 @@
+from fpdf import FPDF as pdf
+from pathlib import Path
+from json import loads
+
+ROOTDIR = Path(__file__).resolve().parent.parent
+
+class CV(pdf):
+    '''
+    call upon the class CV to create you cv based off the infomation in the
+    settings.json file. to know how to input info to settings.json refer
+    back to the github page of this project please
+    '''
+
+    def initialise(self):
+        try:
+            file = open(ROOTDIR / 'settings.json', 'r')
+            self.data = loads(file.read())
+            self.settings = self.data['settings']
+            file.flush()
+            file.close()
+
+            if not Path(ROOTDIR / 'assets/img/link.png').is_file():
+                print('Could not find the image file link.png in assets/img')
+                print('Exiting program...')
+                quit(-1)
+
+            fill = self.settings['background']
+            color = self.settings['color']
+            self.add_page()
+            self.set_fill_color(fill[0], fill[1], fill[2])
+            self.set_text_color(color[0], color[1], color[2])
+            
+
+        except:
+            print('Could not find settings.json file in the root directory')
+            print('Exiting program...')
+            quit(-1)
+
+    def createtopic(self, name, lines : int = 0):
+        self.set_font(self.settings['font_family'], 'B', int(self.settings['topic_font_size']))
+        height = int(self.settings['topic_font_size']) * 0.5
+        self.cell(0, height, name, fill = True)
+        self.ln(height)
+        self.ln(lines)
+
+    def createtitle(self):
+        self.createtopic(self.data['name'], 10)
+
+    def createcontact(self):
+        #This function is used to create the contact section of the cv
+        contacts = self.data['contact']
+        self.createtopic('Contact')
+        self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+
+        for item in contacts:
+            info = '{} : {} '.format(item['type'], item['name'])
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+            self.cell(5)
+            x = self.get_x() 
+            y = self.get_y() + int(height * 0.25)
+
+            if item['link'].strip() != '':
+                image_size = int(int(self.settings['sentence_font_size']) * 0.3)
+                self.image(str(ROOTDIR / 'assets/img/link.png'), x = x, y = y, w = image_size, h = image_size, link = item['link'])
+
+            self.ln(height)
+
+        self.ln(10)
+
+    def createeducation(self):
+        #This function is used to create the contact section of the cv
+        eduction = self.data['education']
+        self.createtopic('Education')
+        self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+
+        for item in eduction:
+            info = '{} :    {}      '.format(item['name'], item['institution'])
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+
+            self.set_font(self.settings['font_family'], 'I', int(self.settings['sentence_font_size']))
+            self.write(height, '{} to {} '.format(item['start_date'], item['end_date']))
+
+            self.cell(5)
+            x = self.get_x() 
+            y = self.get_y() + int(height * 0.25)
+
+            if item['link'].strip() != '':
+                image_size = int(int(self.settings['sentence_font_size']) * 0.3)
+                self.image(str(ROOTDIR / 'assets/img/link.png'), x = x, y = y, w = image_size, h = image_size, link = item['link'])
+
+            self.ln(height)
+
+        self.ln(10)
+
+    def createexperience(self):
+        #This function is used to create the contact section of the cv
+        experience = self.data['experience']
+        self.createtopic('Experience')
+
+        for item in experience:
+            self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+            
+            tools = ' | '.join(item['tools'])
+            info = '{} :    {}     '.format(item['name'], tools)
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+
+            self.set_font(self.settings['font_family'], 'I', int(self.settings['sentence_font_size']))
+            self.write(height, '{} to {}   '.format(item['start_date'], item['end_date']))
+
+            self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+            self.write(height, item['work_place'])
+
+            self.cell(5)
+            x = self.get_x() 
+            y = self.get_y() + int(height * 0.25)
+
+            if item['link'].strip() != '':
+                image_size = int(int(self.settings['sentence_font_size']) * 0.3)
+                self.image(str(ROOTDIR / 'assets/img/link.png'), x = x, y = y, w = image_size, h = image_size, link = item['link'])
+
+            self.ln(height)
+            self.ln(10)
+
+    def createskill(self):
+        #This function is used to create the contact section of the cv
+        skills = self.data['skills']
+        self.createtopic('Skills')
+        self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+
+        for item in skills:
+
+            libs = ' | '.join(item['libraries'])
+            info = '{} :    {}'.format(item['name'], libs)
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+            self.ln(height)
+
+        self.ln(10)
+        
+    def createachivement(self):
+        #This function is used to create the contact section of the cv
+        achivements = self.data['achievments']
+        self.createtopic('Achivements')
+        self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+
+        for item in achivements:
+            info = '{} :    {}'.format(item['name'], item['description'])
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+            self.cell(5)
+            x = self.get_x() 
+            y = self.get_y() + int(height * 0.25)
+
+            if item['link'].strip() != '':
+                image_size = int(int(self.settings['sentence_font_size']) * 0.3)
+                self.image(str(ROOTDIR / 'assets/img/link.png'), x = x, y = y, w = image_size, h = image_size, link = item['link'])
+
+            self.ln(height)
+
+        self.ln(10)
+
+    def createprojects(self):
+        #This function is used to create the contact section of the cv
+        projects = self.data['projects']
+        self.createtopic('Achivements')
+        self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+
+        for item in projects:
+            info = '{} :    {}'.format(item['name'], item['description'])
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+            self.cell(5)
+            x = self.get_x() 
+            y = self.get_y() + int(height * 0.25)
+
+            if item['link'].strip() != '':
+                image_size = int(int(self.settings['sentence_font_size']) * 0.3)
+                self.image(str(ROOTDIR / 'assets/img/link.png'), x = x, y = y, w = image_size, h = image_size, link = item['link'])
+
+            self.ln(height)
+            self.ln(10)
+
+    def createlanguages(self):
+        #This function is used to create the contact section of the cv
+        languages = self.data['languages']
+        self.createtopic('Languages')
+        self.set_font(self.settings['font_family'], '', int(self.settings['sentence_font_size']))
+
+        for item in languages:
+            info = '{} :    {}'.format(item['name'], item['description'])
+            height = int(self.settings['sentence_font_size']) * 0.5
+
+            self.write(height, info)
+            self.ln(height)
+
+        self.ln(10)
+
+
+if __name__ == "__main__":
+    """
+    This doesn't need any explaination
+    """
+    pdf = CV()
+    pdf.initialise()
+    pdf.createtitle()
+    pdf.createcontact()
+    pdf.createeducation()
+    pdf.createexperience()
+    pdf.createskill()
+    pdf.createachivement()
+    pdf.createprojects()
+    pdf.createlanguages()
+    pdf.output(ROOTDIR / "cv.pdf", "F")
